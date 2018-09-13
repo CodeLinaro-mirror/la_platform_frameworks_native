d05269f69c6a7fe7b21ed6299ea5f9d83bb49fad
Jayant Chowdhary <jchowdhary@google.com>
2018-09-13 16:30:03 -0700

    IPCThreadState: Add a public method to probe if a binder call is being served.
    This can be useful when common code can be executed by both hwbinder and
    binder threads, and, they want to access (hardware::)IPCThreadState
    specific information. Eg: cameraserver.
    
    This commit adds a vndk private library 'libbinderthreadstate' which
    serves as common ground for both libbinder and libhwbinder to inform
    them about the nature of the IPC call being served (if at all) on the current
    thread.
    
    Bug: 110364143
    
    Test: Boot device, use GCA
    
    (cherry pick from commit 0397face57629e778b2bafe276afa0ca7479236b)
    
    Change-Id: Ifefb273c9a1f545417a82e9511a45bda00f701aa
    Signed-off-by: Jayant Chowdhary <jchowdhary@google.com>

--- libs/binder/IPCThreadState.cpp
+++ libs/binder/IPCThreadState.cpp
@@ -17,6 +17,7 @@
 #define LOG_TAG "IPCThreadState"
 #define LOG_NDEBUG 1
 #include <binder/IPCThreadState.h>
+#include <binder/IPCThreadStateBase.h>
 
 #include <binder/Binder.h>
 #include <binder/BpBinder.h>
@@ -695,6 +696,7 @@ IPCThreadState::IPCThreadState()
     clearCaller();
     mIn.setDataCapacity(256);
     mOut.setDataCapacity(256);
+    mIPCThreadStateBase = IPCThreadStateBase::self();
 }
 
 IPCThreadState::~IPCThreadState()
@@ -1033,6 +1035,9 @@ status_t IPCThreadState::executeCommand(int32_t cmd)
                 "Not enough command data for brTRANSACTION");
             if (result != NO_ERROR) break;
 
+            //Record the fact that we're in a binder call.
+            mIPCThreadStateBase->pushCurrentState(
+                IPCThreadStateBase::CallState::BINDER);
             Parcel buffer;
             buffer.ipcSetDataReference(
                 reinterpret_cast<const uint8_t*>(tr.data.ptr.buffer),
@@ -1080,6 +1085,7 @@ status_t IPCThreadState::executeCommand(int32_t cmd)
                 error = the_context_object->transact(tr.code, buffer, &reply, tr.flags);
             }
 
+            mIPCThreadStateBase->popCurrentState();
             //ALOGI("<<<< TRANSACT from pid %d restore pid %d uid %d\n",
             //     mCallingPid, origPid, origUid);
 
@@ -1141,6 +1147,10 @@ status_t IPCThreadState::executeCommand(int32_t cmd)
     }
 
     return result;
+}
+
+bool IPCThreadState::isServingCall() const {
+    return mIPCThreadStateBase->getCurrentBinderCallState() == IPCThreadStateBase::CallState::BINDER;
 }
 
 void IPCThreadState::threadDestructor(void *st)
