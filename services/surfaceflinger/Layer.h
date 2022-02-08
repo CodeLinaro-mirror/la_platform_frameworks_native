417b2f1ee3102d145337ba7ed400c5e829f24593
Vishnu Nair <vishnun@google.com>
2022-02-08 02:53:27 +0000

    SurfaceFlinger: Implement drop input modes
    ALL: If this mode is set on a layer, set the DROP_INPUT
    feature flag on the layer and its children if it has a valid input
    channel. This will ensure these layers will not be able to receive any
    input.
    
    Note: this backport does not include the OBSCURED option since its
    not needed for the security fix.
    
    Test: atest libgui_test InputDispatcherDropInputFeatureTest
    Bug: 197296414
    
    Merged-In: Ibce11ee7df5b5c8c226ebfab29574a99cd656f6d
    Change-Id: Ibce11ee7df5b5c8c226ebfab29574a99cd656f6d
    (cherry picked from commit cbe9c105fcdeba79f1722616e8b576c1d45c93a6)
    Merged-In: Ibce11ee7df5b5c8c226ebfab29574a99cd656f6d

--- services/surfaceflinger/Layer.h
+++ services/surfaceflinger/Layer.h
@@ -1075,6 +1075,9 @@ private:
     void updateTreeHasFrameRateVote();
     bool isTrustedOverlay() const;
 
+    gui::DropInputMode getDropInputMode() const;
+    void handleDropInputMode(InputWindowInfo& info) const;
+
     // Cached properties computed from drawing state
     // Effective transform taking into account parent transforms and any parent scaling.
     ui::Transform mEffectiveTransform;
