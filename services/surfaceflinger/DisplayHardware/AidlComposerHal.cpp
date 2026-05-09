fc13cbc3a8754aa25f620eccff75f4e01bbcbb2e
Kartik Mathur <kartmath@qti.qualcomm.com>
2026-05-08 18:30:18 -0700

    platform/frameworks/native: Adding comments to QC value adds
    This commit adds comments to all files with current
    value adds in the project describing the date,
    tech area & commit title. This will help customers
    in identifying value adds in AOSP Projects.
    
    Format:
    // QTI_BEGIN: <date> <tech area> <commit title>
    <value adds>
    // QTI_END: <date> <tech area> <commit title>
    
    CRs-Fixed: 4039187
    Change-Id: Ib5496a053995470bd3781e059982fe4ecfdc338d

--- services/surfaceflinger/DisplayHardware/AidlComposerHal.cpp
+++ services/surfaceflinger/DisplayHardware/AidlComposerHal.cpp
@@ -946,6 +946,7 @@ Error AidlComposer::presentOrValidateDisplay(Display display, nsecs_t expectedPr
 
     *state = translate<uint32_t>(*result);
 
+// QTI_BEGIN: 2024-02-28: Display: AidlComposerHal: Add handling for presentOrValidatedisplay state
     if (*state == 2) {
         auto fence = reader->get().takePresentFence(displayId);
         // take ownership
@@ -954,6 +955,7 @@ Error AidlComposer::presentOrValidateDisplay(Display display, nsecs_t expectedPr
         reader->get().hasChanges(displayId, outNumTypes, outNumRequests);
     }
 
+// QTI_END: 2024-02-28: Display: AidlComposerHal: Add handling for presentOrValidatedisplay state
     if (*result == PresentOrValidate::Result::Presented) {
         auto fence = reader->get().takePresentFence(displayId);
         // take ownership
